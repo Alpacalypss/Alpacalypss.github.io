b1d57553db6fb163c40a24d233c148570e54d297
Cain Bailey <cainbailey1993@gmail.com>
2024-04-05 23:07:11 -0700

    Finished styling, and adding button functionality

--- src/components/History/history.js
+++ src/components/History/history.js
@@ -4,45 +4,48 @@ import "./history.css";
 const History = () => {
   return (
     <>
-    <section id="history">
-      <h1 className="historyTitle">A Brief Overview of Me</h1>
-      <span className="historyMe">
-        For most of my life I have bounced around from hobby to hobby. My eclectic list of hobbies includes:
-        <ul className="list">
-          <li className="bullet">Landscaping</li> 
-          <li className="bullet">Skateboarding</li>
-          <li className="bullet">Cooking</li>
-          <li className="bullet">Gymnastics</li>
-          <li className="bullet">Rock Climbing</li>
-          <li className="bullet">Fire Spinning</li>
-          <li className="bullet">Gaming(all kinds!)</li> 
-          <li className="bullet">And plenty more!</li>
-        </ul> 
-        landscaping, skateboarding, cooking, gymnastics,
-        rockclimbing, fire spinning, and gaming, as well as many others. My
-        favorite thing in the world is constantly being able to learn and expand
-        on things I already know. During and right out of highschool I began
-        going to the University of Oregon focusing on three majors: Physics,
-        Math, and Engineering. I was enjoying the schooling aspect of it, but
-        the further I got, the more unsure I felt about commiting myself to
-        typical engineering as a career. I was left with a choice, between going
-        into debt to finish my degree in something I was no longer feeling
-        positively about, or get a job and try something new. I chose the
-        latter, and at 19 started working as a dishwasher for a restaurant
-        called McMenamins Northbank. Within a year of starting there, I was
-        promoted into management, as the youngest manager in company history. I
-        loved working there and learned so many invaluable skills, but as time
-        went on I started to feel the complacency set in. I was no longer
-        learning new skills, and really missed getting to use my brain
-        creatively to solve problems like I did in school. I knew I did not want
-        to continue pursuing my physics/engineering degree, so I decided to
-        check out computer science, and software engineering. I immediately was
-        in love with the field. All of the sudden I went from bored and unchallenged, to seeing
-        something with endless possibilities that I could spend the rest of my
-        life learning and never truly master all aspects of.
-      </span>
-    </section>
-    <hr></hr>
+      <section id="history">
+        <h1 className="historyTitle">A Brief Overview of Me</h1>
+        <span className="historyMe">
+          For most of my life I have bounced around from hobby to hobby. My
+          eclectic list of hobbies includes:
+          <ul className="list">
+            <li className="bullet">Landscaping</li>
+            <li className="bullet">Skateboarding</li>
+            <li className="bullet">Cooking</li>
+            <li className="bullet">Gymnastics</li>
+            <li className="bullet">Rock Climbing</li>
+            <li className="bullet">Fire Spinning</li>
+            <li className="bullet">Gaming(all kinds!)</li>
+            <li className="bullet">And plenty more!</li>
+          </ul>
+          My favorite thing in the world is constantly being able to learn and
+          expand on things I already know. During and right out of highschool I
+          began going to the University of Oregon focusing on three majors:
+          Physics, Math, and Engineering. I was enjoying the schooling aspect of
+          it, but the further I got, the more unsure I felt about commiting
+          myself to typical engineering as a career. I was left with a choice,
+          between going into debt to finish my degree in something I was no
+          longer feeling positively about, or get a job and try something new. I
+          chose the latter, and at 19 started working as a dishwasher for a
+          restaurant called McMenamins Northbank. Within a year of starting
+          there, I was promoted into management, as the youngest manager in
+          company history. I loved working there and learned so many invaluable
+          skills, but as time went on I started to feel the complacency set in.
+          I was no longer learning new skills, and really missed getting to use
+          my brain creatively to solve problems like I did in school. I knew I
+          did not want to continue pursuing my physics/engineering degree, so I
+          decided to check out computer science, and software engineering. I
+          immediately fell in love with the field. All of the sudden I went from
+          bored and unchallenged, to seeing something with endless possibilities
+          that I could spend the rest of my life learning and never truly master
+          all aspects of. I constantly wake up with an itinerary of things I get
+          to learn that day. I always have new projects that I am excited to
+          work on, and I've never been so sure that I picked the correct path
+          for myself.
+        </span>
+      </section>
+      <hr></hr>
     </>
   );
 };
